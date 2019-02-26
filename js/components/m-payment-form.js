30155ddd05e6a63548d98591c66d481743273357
Macguire Rintoul <macguire.rintoul@gmail.com>
2019-02-26 12:47:18 -0800

    add template to payment form

--- js/components/m-payment-form.js
+++ js/components/m-payment-form.js
@@ -1,5 +1,72 @@
 class PaymentForm extends HTMLElement {
+  constructor() {
+    super();
+    this.template = `
+    <h2>Payment Details</h2>
+    <form action="#">
+      <div class="col-6">
+        <h4>Shipping Info</h4>
+        <div class="input-wrapper">
+          <label for="name">Name</label><input type="text" id="name">
+        </div>
+        <div class="input-wrapper">
+          <label for="email">Email</label><input type="text" id="email">
+        </div>
+        <div class="input-wrapper">
+          <label for="address">Address</label><input type="text" id="address">
+        </div>
+        <div class="input-wrapper">
+          <label for="city">City</label><input type="text" id="city">
+        </div>
+        <div class="input-wrapper">
+          <label for="province">Province</label><input type="text" id="province">
+        </div>
+        <div class="input-wrapper">
+          <label for="postal">Postal</label><input type="text" id="postal">
+        </div>
+      </div>
+      <div class="col-6">
+        <h4>Billing Info</h4>
+        <div class="input-wrapper">
+          <label>Cards Accepted: </label>
+          <div class="cards">
+            <input type="radio" name="cardtype" id="visa">
+            <i class="fab fa-cc-visa"></i>
+            <label for="visa">Visa</label>
 
+            <input type="radio" name="cardtype" id="amex">
+            <i class="fab fa-cc-amex"></i>
+            <label for="amex">Amex</label>
+
+            <input type="radio" name="cardtype" id="mastercard">
+            <i class="fab fa-cc-mastercard"></i>
+            <label for="mastercard">MasterCard</label>
+
+            <input type="radio" name="cardtype" id="discover">
+            <i class="fab fa-cc-discover"></i>
+            <label for="discover">Discover</label>
+          </div>
+        </div>
+        <div class="input-wrapper">
+          <label for="cname">Name on Card</label><input type="text" id="cname">
+        </div>
+        <div class="input-wrapper">
+          <label for="cc">Credit Card</label><input type="text" id="cc">
+        </div>
+        <div class="input-wrapper">
+          <label for="emonth">Exp Month</label><input type="text" id="emonth">
+        </div>
+        <div class="input-wrapper">
+          <label for="eyear">Exp Year</label><input type="text" id="eyear">
+        </div>
+        <div class="input-wrapper">
+          <label for="cvv">CVV</label><input type="text" id="cvv">
+        </div>
+      </div>
+      <button>Checkout</button>
+    </form>
+    `
+  }
     connectedCallback() {
         /*html*/
         var template = `
@@ -35,8 +102,6 @@ class PaymentForm extends HTMLElement {
                 <i class="fab fa-cc-visa"></i>
                 <label for="visa">Visa</label>
 
-
-
                 <input type="radio" name="cardtype" id="amex">
                 <i class="fab fa-cc-amex"></i>
                 <label for="amex">Amex</label>
@@ -60,7 +125,7 @@ class PaymentForm extends HTMLElement {
               <label for="emonth">Exp Month</label><input type="text" id="emonth">
             </div>
             <div class="input-wrapper">
-              <label for="emonth">Exp Year</label><input type="text" id="eyear">
+              <label for="eyear">Exp Year</label><input type="text" id="eyear">
             </div>
             <div class="input-wrapper">
               <label for="cvv">CVV</label><input type="text" id="cvv">
