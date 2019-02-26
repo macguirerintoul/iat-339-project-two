a4b704774175d46f767c65882d83c36596275c3d
Macguire Rintoul <macguire.rintoul@gmail.com>
2019-02-26 14:10:42 -0800

    add some elements to the style guide

--- js/components/m-cart.js
+++ js/components/m-cart.js
@@ -3,15 +3,16 @@ class Cart extends HTMLElement {
   connectedCallback() {
     /*html*/
     var template = `
-<div class="checkout-cart">
-  <h3><i class="fa fa-shopping-cart"></i> Cart</h3>
-  <div class="items">
-    <i class="fas fa-tshirt"></i>
-    <a class="link" href="#">Moloch T-Shirt</a>
-    <div class="info"> x2</div>
-  </div>
-  <div>Total: 120</div>
-</div>`.trim();
+      <div class="checkout-cart">
+        <h3><i class="fa fa-shopping-cart"></i> Cart</h3>
+        <div class="items">
+          <i class="fas fa-tshirt"></i>
+          <a class="link" href="#">Moloch T-Shirt</a>
+          <div class="info"> x2</div>
+        </div>
+        <div>Total: 120</div>
+      </div>
+    `
     this.innerHTML = template;
   }
 };
