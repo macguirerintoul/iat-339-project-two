103a801ac7a3bde6a3cbc218049ad5fd062e2a51
Josh Stuible <shrewdlove@gmail.com>
2019-02-26 15:08:29 -0800

    Opted for hardcoding the baseURL of our github site so it works with github pages
    Signed-off-by: Josh Stuible <shrewdlove@gmail.com>

--- js/components/m-header.js
+++ js/components/m-header.js
@@ -14,11 +14,11 @@ class HeaderElement extends HTMLElement {
         </a>
       </div>
       <nav>
-        <a class="link-item" href="./">Home</a>
-        <a class="link-item" href="./music/">Music</a>
-        <a class="link-item" href="./about/">About</a>
-        <a class="link-item" href="./contact/">Contact</a>
-        <a class="link-item" href="./styleguide/">Style Guide</a>
+        <a class="link-item" href="/iat-339-project-two/">Home</a>
+        <a class="link-item" href="/iat-339-project-two/music/">Music</a>
+        <a class="link-item" href="/iat-339-project-two/about/">About</a>
+        <a class="link-item" href="/iat-339-project-two/contact/">Contact</a>
+        <a class="link-item" href="/iat-339-project-two/styleguide/">Style Guide</a>
       </nav>
     </header>
     `
