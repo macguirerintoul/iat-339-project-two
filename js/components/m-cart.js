b018a783af0cecd510536b53a7e4696fad8b19c0
Macguire Rintoul <macguire.rintoul@gmail.com>
2019-02-26 13:59:41 -0800

    format cart template literal

--- js/components/m-cart.js
+++ js/components/m-cart.js
@@ -1,27 +1,19 @@
 class Cart extends HTMLElement {
 
-    static get observedAttributes() {
-        return ['image', 'name', 'price'];
-    }
-
-    connectedCallback() {
-        this.image = this.getAttribute('image');
-        this.name = this.getAttribute('name');
-        this.price = this.getAttribute('price');
-        /*html*/
-        var template = `
-        <div class="checkout-cart">
-          <h3><i class="fa fa-shopping-cart"></i> Cart</h3>
-          <div class="items">
-            <i class="fas fa-tshirt"></i>
-            <a class="link" href="#">Moloch T-Shirt</a>
-            <div class="info"> x2</div>
-          </div>
-          <div>Total: 120</div>
-        </div>
-        `
-        this.innerHTML = template;
-    }
+  connectedCallback() {
+    /*html*/
+    var template = `
+<div class="checkout-cart">
+  <h3><i class="fa fa-shopping-cart"></i> Cart</h3>
+  <div class="items">
+    <i class="fas fa-tshirt"></i>
+    <a class="link" href="#">Moloch T-Shirt</a>
+    <div class="info"> x2</div>
+  </div>
+  <div>Total: 120</div>
+</div>`.trim();
+    this.innerHTML = template;
+  }
 };
 
 customElements.define('m-cart', Cart);
