4971b63f7933b0eab4508131b5e5bf43e6f7913c
Macguire Rintoul <macguire.rintoul@gmail.com>
2019-03-04 18:19:39 -0800

    fix #38 - divide style.css into sections

--- js/components/m-cart.js
+++ js/components/m-cart.js
@@ -2,11 +2,9 @@
   const template = document.createElement('template');
 
   template.innerHTML = `
-    <div class="checkout-cart">
-      <h3><i class="fa fa-shopping-cart"></i> Cart</h3>
-      <slot name="cart-items"></slot>
-      <slot name="total"></slot>
-    </div>
+    <h3><i class="fa fa-shopping-cart"></i> Cart</h3>
+    <slot name="cart-items"></slot>
+    <slot name="total">Total: $99.69</slot>
   `;
 
   class Cart extends HTMLElement {
