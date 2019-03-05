2f802891da452b579e4ecac95a5acb742634813f
Macguire Rintoul <macguire.rintoul@gmail.com>
2019-03-04 17:45:35 -0800

    WIP partially implement cart-items

--- js/components/m-cart.js
+++ js/components/m-cart.js
@@ -5,12 +5,8 @@ class Cart extends HTMLElement {
     var template = `
       <div class="checkout-cart">
         <h3><i class="fa fa-shopping-cart"></i> Cart</h3>
-        <div class="items">
-          <i class="fas fa-tshirt"></i>
-          <a class="link" href="#">Moloch T-Shirt</a>
-          <div class="info"> x2</div>
-        </div>
-        <div>Total: 120</div>
+        <slot name="cart-items"></slot>
+        <slot name="total"></slot>
       </div>
     `
     this.innerHTML = template;
@@ -18,3 +14,7 @@ class Cart extends HTMLElement {
 };
 
 customElements.define('m-cart', Cart);
+
+// <i class="fas fa-tshirt"></i>
+// <a class="link" href="#">Moloch T-Shirt</a>
+// <div class="info"> x2</div> -->
