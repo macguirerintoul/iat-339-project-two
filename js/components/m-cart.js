56c263d13dbaf274c122dc58f538e6884de39d8b
Macguire Rintoul <macguire.rintoul@gmail.com>
2019-03-05 11:29:40 -0800

    fix #42, Cart undefined

--- js/components/m-cart.js
+++ js/components/m-cart.js
@@ -1,21 +1,19 @@
-(function() {
-  const template = document.createElement('template');
+const cartTemplate = document.createElement('template');
 
-  template.innerHTML = `
-    <div class="checkout-cart">
-      <h3><i class="fa fa-shopping-cart"></i> Cart</h3>
-      <slot name="cart-items"></slot>
-      <slot name="total"></slot>
-    </div>
-  `;
+cartTemplate.innerHTML = `
+  <div class="checkout-cart">
+    <h3><i class="fa fa-shopping-cart"></i> Cart</h3>
+    <slot name="cart-items"></slot>
+    <slot name="total"></slot>
+  </div>
+`;
 
-  class Cart extends HTMLElement {
-    constructor() {
-      super();
-      this.attachShadow({ mode: 'open' });
-      this.shadowRoot.appendChild(template.content.cloneNode(true));
-    }
-  };
+class Cart extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(cartTemplate.content.cloneNode(true));
+  }
+};
 
-  customElements.define('m-cart', Cart);
-})();
+customElements.define('m-cart', Cart);
